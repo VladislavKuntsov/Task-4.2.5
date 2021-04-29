347c3685fc09c95040115ffc598bff069c449350
Vladislav Kuntsov <vladislav.kuntsov@gmail.com>
2021-04-29 09:31:57 +0300

    Update test.js
    Ок

--- test.js
+++ test.js
@@ -109,5 +109,7 @@ function addRepositories(data) {   // Добавляем информацию п
 }
  
 informSearch.addEventListener("click", (event) => {
-    event.target.parentNode.remove();
+    if(event.target.classList.contains("close")) {
+      event.target.parentNode.remove();
+    }
 })
